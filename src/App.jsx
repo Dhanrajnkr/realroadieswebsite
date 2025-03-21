c01f038c930c1a8670fc82452912b535879d0fe5
Dhanrajnkr <dr07012002@gmail.com>
2025-03-21 13:16:40 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import NavbarSection from './components/navbar/Navbar';
 import Footer from './components/footer/Footer';
-import HeroSection from "./pages/Home/HeroSection";
-import TrainingHome from './pages/Getaways/trainings/TrainingHome';
-import Getaways from './pages/Getaways/Getaways';
-import About from './pages/About/About';
-import CorporateEvent from './pages/Corporateevent/CorporateEvent';
-import AdventureToursDetails from './pages/Getaways/Adventuretours/AdventureToursDetails';
-import Contact from './pages/Contact/Contact';
-import Details from './pages/Getaways/details/Details';
-import TermsConditions from './pages/Company/terms&conditions/TermsConditions';
-import RefundPolicy from './pages/Company/refund-policy/RefundPolicy';
-import PrivacyPolicy from './pages/Company/privacy-policy/PrivacyPolicy';
+import HeroSection from "./pages/home/HeroSection";
+import TrainingHome from './pages/getaways/trainings/TrainingHome';
+import Getaways from './pages/getaways/Getaways';
+import About from './pages/about/About';
+import CorporateEvent from './pages/corporateevent/CorporateEvent';
+import AdventureToursDetails from './pages/getaways/Adventuretours/AdventureToursDetails';
+import Contact from './pages/contact/Contact';
+import Details from './pages/getaways/details/Details';
+import TermsConditions from './pages/company/terms&conditions/TermsConditions';
+import RefundPolicy from './pages/company/refund-policy/RefundPolicy';
+import PrivacyPolicy from './pages/company/privacy-policy/PrivacyPolicy';
 
 const App=() => {
   return (
