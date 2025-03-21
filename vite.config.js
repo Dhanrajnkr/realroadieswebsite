bb8da9345717013467ae29c35ac4fd93f2725b41
Dhanrajnkr <dr070120022@gmail.com>
2025-03-21 16:18:53 +0530

    good morning

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,32 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       '/api': 'https://dev-api.realroadies.com', // Adjust to your backend's local URL
+//     }
+//   }
+  
+// })
+
+
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'https://dev-api.realroadies.com', // Adjust to your backend's local URL
-    }
-  }
-  
-})
+      "/api": {
+        target: "https://dev-api.realroadies.com", // Change this to your real backend URL
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
+
